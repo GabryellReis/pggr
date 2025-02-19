276f32d2e64262c95f4b1adc61cec607e646d9ea
= <=>
2025-02-19 07:30:24 -0300

    fix: corrige importacoes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ShinyText from './components/shiny-text/ShinyText'
 import TextPressure from './components/pressure/TextPressure'
